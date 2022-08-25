8e1be0256f82687b5b14c45643fc021a2a828271
Darllinson Azevedo <azevedo.darllinson@gmail.com>
2022-08-25 10:59:58 -0300

    Add categories

--- ouvidoria/ouvidoria-darllinson.py
+++ ouvidoria/ouvidoria-darllinson.py
@@ -4,6 +4,8 @@ username = input("Digite seu nome: ")
 userId = int(input("Digite sua matrícula: "))
 
 claims = []  # Array de reclamações
+ideas = []  # Array de ideias
+others = []  # Array de outros feedbacks
 
 print("\nBem-vindo %s!" % username.capitalize())
 print("Matrícula: %d" % userId)
@@ -11,9 +13,9 @@ print("Matrícula: %d" % userId)
 while True:
     print("-" * 75)
     print("\nSelecione uma opção:\n")
-    print("1 - Listar reclamações")
-    print("2 - Adicionar reclamação")
-    print("3 - Remover reclamações")
+    print("1 - Listar feedbacks")
+    print("2 - Adicionar feedback")
+    print("3 - Remover feedbacks")
     print("4 - Sair\n")
     print("-" * 75)
 
@@ -22,43 +24,111 @@ while True:
     if option == "1":
         print()
 
-        if len(claims) == 0:
-            print("\nNão há reclamações para exibir\n")
+        if len(claims) == 0 and len(ideas) == 0 and len(others) == 0:
+            print("\nNão há feedbacks para exibir\n")
         else:
+            print("\nReclamações:")
+            print("\nNão há reclamações" if len(claims) == 0 else "")
             for index, claim in enumerate(claims):
                 print(index + 1, "|", claim)
+
+            print("\nIdeias:")
+            print("\nNão há ideias" if len(ideas) == 0 else "")
+            for index, idea in enumerate(ideas):
+                print(index + 1, "|", idea)
+
+            print("\nOutros:")
+            print("\nNão há outros feedbacks" if len(others) == 0 else "")
+            for index, other in enumerate(others):
+                print(index + 1, "|", other)
     elif option == "2":
-        claim = input("\nDigite sua reclamação:\n")
-        claims.append(claim)
-        print("\nReclamação adicionada com sucesso!")
-    elif option == "3":
-        deleteAll = input("\nDeseja apagar todas as reclamações? (s/N) ")
+        print("\nCategorias:\n")
+        print("1 - Reclamação")
+        print("2 - Ideia")
+        print("3 - Outro")
 
-        if (
-            deleteAll == "s"
-            or deleteAll == "S"
-            or deleteAll == "sim"
-            or deleteAll == "Sim"
-            or deleteAll == "SIM"
-        ):
-            claims.clear()
-            print("\nTodas as reclamações foram removidas!")
+        category = input("Qual a categoria do seu feedback? (1 / 2 / 3) ")
+
+        if category == "1":
+            claim = input("\nDigite sua reclamação:\n")
+            claims.append(claim)
+            print("\nReclamação adicionada com sucesso!")
+        elif category == "2":
+            idea = input("\nDigite sua ideia:\n")
+            ideas.append(idea)
+            print("\nIdeia adicionada com sucesso!")
+        elif category == "3":
+            other = input("\nDigite seu feedback:\n")
+            others.append(other)
+            print("\nFeedback adicionado com sucesso!")
         else:
-            print("\nQual a reclamação que você deseja remover?\n")
+            print("\nCategoria não encontrada!\n")
+    elif option == "3":
+        deleteAll = input("\nDeseja apagar todas os feedbacks? (s/N) ")
 
-            if len(claims) == 0:
-                print("\nNão há reclamações para remover")
-            else:
-                for index, claim in enumerate(claims):
-                    print(index + 1, "|", claim)
+        if deleteAll == "s" or deleteAll == "S":
+            claims.clear()
+            ideas.clear()
+            others.clear()
+            print("\nTodas os feedbacks foram removidos!")
+        else:
+            print("\nCategorias:\n")
+            print("1 - Reclamação")
+            print("2 - Ideia")
+            print("3 - Outro")
 
-            indexOfClaim = int(input("\nNúmero da reclamação: "))
+            category = input(
+                "Qual a categoria do feedback que deseja remover? (1 / 2 / 3) "
+            )
 
-            if indexOfClaim > len(claims) or indexOfClaim <= 0:
-                print("\nNão encontramos a reclamação em nosso banco de dados!\n")
-            else:
-                claims.pop(indexOfClaim - 1)
-                print("\nReclamação removida com sucesso!\n")
+            if category == "1":
+                print("\nQual a reclamação que você deseja remover?\n")
+
+                if len(claims) == 0:
+                    print("\nNão há reclamações para remover")
+                else:
+                    for index, claim in enumerate(claims):
+                        print(index + 1, "|", claim)
+
+                indexOfClaim = int(input("\nNúmero da reclamação: "))
+
+                if indexOfClaim > len(claims) or indexOfClaim <= 0:
+                    print("\nNão encontramos a reclamação em nosso banco de dados!\n")
+                else:
+                    claims.pop(indexOfClaim - 1)
+                    print("\nReclamação removida com sucesso!\n")
+            elif category == "2":
+                print("\nQual a ideia que você deseja remover?\n")
+
+                if len(ideas) == 0:
+                    print("\nNão há ideias para remover")
+                else:
+                    for index, idea in enumerate(ideas):
+                        print(index + 1, "|", idea)
+
+                indexOfClaim = int(input("\nNúmero da ideia: "))
+
+                if indexOfClaim > len(ideas) or indexOfClaim <= 0:
+                    print("\nNão encontramos a ideia em nosso banco de dados!\n")
+                else:
+                    ideas.pop(indexOfClaim - 1)
+                    print("\nIdeia removida com sucesso!\n")
+            elif option == "3":
+                print("\nQual o feedback que você deseja remover?\n")
+
+                if len(other) == 0:
+                    print("\nNão há feedbacks para remover")
+                else:
+                    for index, other in enumerate(others):
+                        print(index + 1, "|", other)
+
+                indexOfClaim = int(input("\nNúmero do feedback: "))
+
+                if indexOfClaim > len(others) or indexOfClaim <= 0:
+                    print("\nNão encontramos o feedback em nosso banco de dados!\n")
+                else:
+                    others.pop(indexOfClaim - 1)
+                    print("\nFeedback removido com sucesso!\n")
     elif option == "4":
         print("\nObrigado por usar o sistema de ouvidoria da Unifacisa!")
         break
